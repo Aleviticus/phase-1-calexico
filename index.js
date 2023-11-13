90c8488f57ebf8a24da59e127d52672114637f18
Aaron Levy <aaronlevypc21@gmail.com>
2023-11-13 10:17:22 -0500

    coding

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here...
+fetch("http://localhost:3000/menu")
+.then((res) => res.json())
+.then((data) => {
+    console.log(data);
+    for(let i of data) {
+        renderImage(data)
+    }
+});
+
+function renderImage(data) {
+
+}
